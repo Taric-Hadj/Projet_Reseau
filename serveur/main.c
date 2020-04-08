7812ff3e47fc6a129b514632706335df634c9552
Zulgurub83 <Zulgurub83@users.noreply.github.com>
2020-04-08 11:23:24 +0200

    include et le main.c n'est plus vide Benjamin

---
+++ serveur/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> /* pour exit */
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h> /* pour exit */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h> /* pour memset */
+#include <netinet/in.h> /* pour struct sockaddr_in */
+#include <arpa/inet.h> /* pour htons et inet_aton */
+#include <unistd.h> /* pour sleep */
+
+int main(int argc, char const *argv[]) {
