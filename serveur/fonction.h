3ccd1030296789b3ade443c4fb081f4808cdd2ea
Taric-Hadj <59012292+Taric-Hadj@users.noreply.github.com>
2020-05-20 10:11:03 +0200

    ca c'esr carré

--- serveur/fonction.h
+++ serveur/fonction.h
@@ -1,8 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> /* pour exit */
+#include <unistd.h> /* pour close */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h> /* pour memset */
+#include <netinet/in.h> /* pour struct sockaddr_in */
+#include <arpa/inet.h> /* pour htons et inet_aton */
+#include <unistd.h> /* pour sleep */
+#include <poll.h>
 
-#define MAX_USERS 10
-#define MAX_LOGIN_SIZE 50
+#define PORT IPPORT_USERRESERVED // = 5000
 
-struct user{
-    int socket;
-    char login[MAX_LOGIN_SIZE];
-};
+
+#define MAX_DIGIMONS 3
+#define LG_MESSAGE   256
+
+typedef struct User {
+	int socketClient;
+	char login [50];
+	}digimon;
--- serveur/main.c
+++ serveur/main.c
@@ -3,58 +3,53 @@
 #include <unistd.h> /* pour close */
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>		/* pour memset */
+#include <string.h> /* pour memset */
 #include <netinet/in.h> /* pour struct sockaddr_in */
-#include <arpa/inet.h>	/* pour htons et inet_aton */
-#include <unistd.h>		/* pour sleep */
+#include <arpa/inet.h> /* pour htons et inet_aton */
+#include <unistd.h> /* pour sleep */
 #include <poll.h>
 #include "fonction.h"
 
-#define PORT IPPORT_USERRESERVED // = 5000
-
-#define LG_MESSAGE 256
 
 int main()
 {
 	int socketEcoute;
 	struct sockaddr_in pointDeRencontreLocal;
 	socklen_t longueurAdresse;
-	int socketDialogue;
 	struct sockaddr_in pointDeRencontreDistant;
-	char messageEnvoi[LG_MESSAGE]; /* le message de la couche Application ! */
-	char messageRecu[LG_MESSAGE];  /* le message de la couche Application ! */
-	int ecrits, lus;			   /* nb d'octets ecrits et lus */
-	int retour;
-	struct user users[MAX_USERS];
-	struct pollfd pollfds[MAX_USERS + 1];
+	char messageEnvoi[LG_MESSAGE]; /* le message de la couche Application ! */  
+	char messageRecu[LG_MESSAGE]; /* le message de la couche Application ! */ 
+	char messageAcceuil[LG_MESSAGE]; 
+	char list[LG_MESSAGE];
+	int ecrits; 
+	int lus; /* nb d'octets ecrits et lus */
+	//int retour;
+	digimon Digimons[MAX_DIGIMONS];
+	struct pollfd pollfds[MAX_DIGIMONS + 1];
 
-<<<<<<< HEAD
-	memset(Digimons, '\0', MAX_DIGIMON * sizeof(struct Digimon));
-=======
-	memset(users, '\0', MAX_USERS*sizeof(struct user));
->>>>>>> parent of da7316a... mode digimon
+	memset(Digimons, '\0', MAX_DIGIMONS*sizeof(digimon));
 
-	// Crée un socket de communication
+	// Crée un socket de communication 
 	socketEcoute = socket(PF_INET, SOCK_STREAM, 0); /* 0 indique que l'on utilisera le protocole par défaut associé à SOCK_STREAM soit TCP */
 
 	// Teste la valeur renvoyée par l'appel système socket()
-	if (socketEcoute < 0) /* échec ? */
+	if(socketEcoute < 0) /* échec ? */
 	{
 		perror("socket"); // Affiche le message d'erreur
-		exit(-1);		  // On sort en indiquant un code erreur
-	}
+		exit(-1); // On sort en indiquant un code erreur
+	} 
 
 	printf("Socket créée avec succès ! (%d)\n", socketEcoute);
 
 	// On prépare l'adresse d'attachement locale
 	longueurAdresse = sizeof(struct sockaddr_in);
 	memset(&pointDeRencontreLocal, 0x00, longueurAdresse);
-	pointDeRencontreLocal.sin_family = PF_INET;
-	pointDeRencontreLocal.sin_addr.s_addr = htonl(INADDR_ANY); // toutes les interfaces locales disponibles
-	pointDeRencontreLocal.sin_port = htons(PORT);
+	pointDeRencontreLocal.sin_family        = PF_INET;
+	pointDeRencontreLocal.sin_addr.s_addr   = htonl(INADDR_ANY); // toutes les interfaces locales disponibles
+	pointDeRencontreLocal.sin_port          = htons(PORT);   
 
 	// On demande l'attachement local de la socket
-	if ((bind(socketEcoute, (struct sockaddr *)&pointDeRencontreLocal, longueurAdresse)) < 0)
+	if((bind(socketEcoute, (struct sockaddr *)&pointDeRencontreLocal, longueurAdresse)) < 0)
 	{
 		perror("bind");
 		exit(-2);
@@ -63,7 +58,7 @@ int main()
 	printf("Socket attachée avec succès !\n");
 
 	// On fixe la taille de la file d'attente à 5 (pour les demande de connexion non encore traitées)
-	if (listen(socketEcoute, 5) < 0)
+	if(listen(socketEcoute, 5) < 0)
 	{
 		perror("listen");
 		exit(-3);
@@ -73,200 +68,178 @@ int main()
 	printf("Socket placée en écoute passive ...\n");
 
 	// boucle d'attente de connexion : en théorie, un serveur attend indéfiniment !
-	while (1)
+	while(1)
 	{
 		int nevents;
 		int nfds = 0;
-
+		
+		memset(messageRecu, 0x00, LG_MESSAGE*sizeof(char));
+		memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+		memset(messageAcceuil, 0x00, LG_MESSAGE*sizeof(char));
 		// Liste des sockets à écouter
-		// socketEcoute + Digimons[].socket => pollfds[]
+		// socketEcoute + users[].socket => pollfds[]
 		pollfds[nfds].fd = socketEcoute;
 		pollfds[nfds].events = POLLIN;
 		pollfds[nfds].revents = 0;
 		nfds++;
-
-		for (int i = 0; i < MAX_DIGIMON; i++)
-		{
-<<<<<<< HEAD
-			if (Digimons[i].socket > 0)
-			{
-				pollfds[nfds].fd = Digimons[i].socket;
+		
+		for(int i = 0 ; i<MAX_DIGIMONS;i++){
+			if(Digimons[i].socketClient > 0){
+				pollfds[nfds].fd = Digimons[i].socketClient;
 				pollfds[nfds].events = POLLIN;
 				pollfds[nfds].revents = 0;
 				nfds++;
+				}
 			}
-		}
 		nevents = poll(pollfds, nfds, -1);
-		if (nevents > 0)
+		if (nevents > 0) 
 		{
 			// parcours de pollfds[] à la recherche des revents != 0
-			for (int j = 0; j < nfds; j++)
+			for(int j =0;j< nfds;j++)
 			{
-				if (pollfds[j].revents != 0)
+				if(pollfds[j].revents != 0)
 				{
-					if (j == 0)
-					{
-						int i = 0;
-						for (i = 0; i < MAX_DIGIMON; i++)
+					if(j == 0)
+					{	
+						int i;
+						for(i=0;i<MAX_DIGIMONS;i++)
 						{
-							if (Digimons[i].socket == 0)
+							if(Digimons[i].socketClient == 0)
 							{
-								Digimons[i].socket = accept(socketEcoute, (struct sockaddr *)&pointDeRencontreDistant, &longueurAdresse);
-
-								snprintf(Digimons[i].login, 50, "DIGIMON %d", i + 1);
-								printf("%s s'est connecté\n\n", Digimons[i].login);
-								if (Digimons[i].socket < 0)
+								Digimons[i].socketClient = accept(socketEcoute,(struct sockaddr *)&pointDeRencontreDistant, & longueurAdresse);
+								
+								snprintf(Digimons[i].login,50, "digimon %d",i+1) ;
+								printf("%s s'est connecté\n\n",Digimons[i].login) ;
+								for(int k=0; k<MAX_DIGIMONS;k++)
+								{
+									if(Digimons[k].socketClient != 0)
+									{	
+										snprintf(messageAcceuil,50, "bienvenue digimon %d\n",i+1);
+										ecrits = write(Digimons[k].socketClient, messageAcceuil, strlen(messageAcceuil));
+										if(ecrits == -1)
+										{
+											perror("write");
+											close(Digimons[k].socketClient);
+											exit(-6);
+										}
+									}
+								}
+								if(Digimons[i].socketClient < 0)
 								{
 									perror("accept");
-									close(Digimons[i].socket);
+									close(Digimons[i].socketClient);
 									close(socketEcoute);
-									exit(-4);
+									exit(-4);						
 								}
 								break;
-							}
+							}	
 						}
-						if (i == MAX_DIGIMON)
+						if(i == MAX_DIGIMONS)
 						{
-							close(Digimons[i].socket);
-							printf("Max d'utilisateur atteint");
+							close(Digimons[i].socketClient);
+							printf("Max d'utilisateur atteint\n");
 							break;
 						}
 					}
 					else
 					{
-						for (int i = 0; i < MAX_DIGIMON; i++)
+						for(int i=0; i<MAX_DIGIMONS;i++)
 						{
-							if (pollfds[j].fd == Digimons[i].socket)
+							if(pollfds[j].fd == Digimons[i].socketClient)
 							{
-								lus = read(Digimons[i].socket, messageRecu, LG_MESSAGE * sizeof(char));
-							/*	for (int w = 0; w < MAX_DIGIMON; w++)
+								lus = read(Digimons[i].socketClient,messageRecu,LG_MESSAGE*sizeof(char));
+								if(strstr(messageRecu,"!version") != NULL)
+								{	
+									memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+									strcpy(messageEnvoi,"Version 1.0\n");
+									write(Digimons[i].socketClient, messageEnvoi, strlen(messageEnvoi));
+								}
+								else if(strstr(messageRecu,"!list") != NULL)
 								{
-									if(Digimons[w].socket !=0)
+									memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+									for(int k=0; k<MAX_DIGIMONS;k++)
 									{
-										if(w != i)
-										{
-										sprintf(messageEnvoi, 50,"Digimon %d: ",i+1);
-										strcat(messageEnvoi,messageRecu);
-										ecrits = write(Digimons[w].socket,messageEnvoi,strlen(messageEnvoi));
-										if(ecrits == -1)
-										{
-											perror('write');
-											close(Digimons[w].socket);
-											exit(-6);
-										}
+										if(Digimons[k].socketClient != 0)
+										{		
+											snprintf(list, 50, "digimon %d|",k+1);
+											strcat(messageEnvoi,list);
 										}
 									}
+									strcat(messageEnvoi,"\n");
+									write(Digimons[i].socketClient, messageEnvoi, strlen(messageEnvoi));
 								}
-								if (strcmp(messageRecu, "!version") == 0)
+								else if(strstr(messageRecu,"!message") != NULL)
 								{
-									sprintf(messageEnvoi, '!version');
-									write(Digimons[i].socket, messageEnvoi, strlen(messageEnvoi));
-									break;
-								}*/
-
-								switch (lus)
+									memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+									strcpy(messageEnvoi,messageRecu);
+									for(int k=9; k<15; k++)
+										messageEnvoi[k] = messageEnvoi[k+1];
+									messageEnvoi[15] = ':';	
+									messageEnvoi[14] = (char)(i+49);
+									write(Digimons[((int)messageRecu[15])-49].socketClient, messageEnvoi, strlen(messageEnvoi));
+								}
+								else
 								{
-								case -1:
-									perror("read");
-									close(Digimons[i].socket);
-									exit(-5);
-								case 0:
-									fprintf(stderr, "%s s'est déconecté\n\n");
-									close(Digimons[i].socket);
-									Digimons[i].socket = 0;
-								default:
-									printf("Message reçu de %s: %s (%d octets)\n\n", Digimons[i].login, messageRecu, lus);
+									memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+									snprintf(messageEnvoi, 50, "digimon %d: ",i+1);
+									strcat(messageEnvoi,messageRecu);
+									for(int k=0; k<MAX_DIGIMONS;k++)
+									{
+										if(Digimons[k].socketClient != 0)
+										{		
+											if(k != i)
+											{	
+													write(Digimons[k].socketClient, messageEnvoi, strlen(messageEnvoi));
+											}
+										}
+									}	
 								}
-=======
-			int nevents,i,j;
-			int nfds = 0;
-
-			// Liste des sockets à écouter
-			// socketEcoute + users[].socket => pollfds[]
-			pollfds[nfds].fd = socketEcoute;
-			pollfds[nfds].events = POLLIN;
-			pollfds[nfds].revents = 0;
-			nfds++;
-
-			for(int i = 0 ; i<MAX_USERS;i++){
-				if(users[i].socket > 0){
-					pollfds[nfds].fd = users[i].socket;
-					pollfds[nfds].events = POLLIN;
-					pollfds[nfds].revents = 0;
-					nfds++;
-					}
-				}
-			nevents = poll(pollfds, nfds, -1);
-			if (nevents > 0) {
-				// parcours de pollfds[] à la recherche des revents != 0
-				for(int j =0;j< nfds;j++){
-					if(pollfds[j].revents != 0){
-						if(j == 0){
-							int i =0;
-							for(i=0;i<MAX_USERS;i++){
-								if(users[i].socket == 0){
-									users[i].socket = accept(socketEcoute,(struct sockaddr *)&pointDeRencontreDistant, & longueurAdresse);
-
-									snprintf(users[i].login,50, "user %d",i+1) ;
-									printf("%s s'est connecté\n\n",users[i].login) ;
-									if(users[i].socket < 0){
-										perror("accept");
-										close(users[i].socket);
-										close(socketEcoute);
-										exit(-4);
-									}
-									break;
+								memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+								switch(lus)
+								{
+									case -1:
+										perror("read");
+										close(Digimons[i].socketClient);
+										exit(-5);
+									case 0 :
+										fprintf(stderr,"%s s'est déconecté\n\n",Digimons[i].login);
+										close(Digimons[i].socketClient);
+										Digimons[i].socketClient = 0;
+										for(int k=0; k<MAX_DIGIMONS;k++)
+										{
+											if(Digimons[k].socketClient != 0)
+											{	
+												strcpy(messageEnvoi,"s'est déconecté\n"); 
+												ecrits = write(Digimons[k].socketClient, messageEnvoi, strlen(messageEnvoi));
+												if(ecrits == -1)
+												{
+													perror("write");
+													close(Digimons[k].socketClient);
+													exit(-6);
+												}
+											}
+										}
+									default:
+										printf("Message reçu de %s: %s (%d octets)\n\n",Digimons[i].login,messageRecu,lus);
 								}
+								
 							}
-							if(i == MAX_USERS)
-							{
-								close(users[i].socket);
-								printf("Max d'utilisateur atteint");
-								break;
-							}
-						}
-			else {
-				for(int i=0; i<MAX_USERS;i++){
-					if(pollfds[j].fd == users[i].socket){
-						lus = read(users[i].socket,messageRecu,LG_MESSAGE*sizeof(char));
-						switch(lus){
-							case -1:
-								perror("read");
-								close(users[i].socket);
-								exit(-5);
-							case 0 :
-								fprintf(stderr,"%s s'est déconecté\n\n");
-								close(users[i].socket);
-								users[i].socket = 0;
-							default:
-								printf("Message reçu de %s: %s (%d octets)\n\n",users[i].login,messageRecu,lus);
->>>>>>> parent of da7316a... mode digimon
-							}
-						}
+						}	
 					}
 				}
 			}
 		}
+		
 
-<<<<<<< HEAD
-		// si c'est la socket socketEcoute => accept() + création d'une nouvelle entrée dans la table Digimons[]
-		//
-		// sinon c'est une socket client => read() et gestion des erreurs pour le cas de la déconnexion
-		else
-		{
+			// si c'est la socket socketEcoute => accept() + création d'une nouvelle entrée dans la table users[]
+			//
+			// sinon c'est une socket client => read() et gestion des erreurs pour le cas de la déconnexion
+		else {
 			printf("poll() returned %d\n", nevents);
-=======
-				// si c'est la socket socketEcoute => accept() + création d'une nouvelle entrée dans la table users[]
-				//
-				// sinon c'est une socket client => read() et gestion des erreurs pour le cas de la déconnexion
-			else {
-				printf("poll() returned %d\n", nevents);
-			}
->>>>>>> parent of da7316a... mode digimon
 		}
 	}
 
-	// On ferme la ressource avant de quitter
+	// On ferme la ressource avant de quitter   
 	close(socketEcoute);
 
 	return 0;
